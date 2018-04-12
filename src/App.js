a5fafbbc39fe11c62ffa8c0d8eade41482864de2
Karthik M S <ms18karthik@gmail.com>
2018-04-12 16:02:44 +0530

    Fetch info from external API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Titles from "./components/Title"
 import Form from "./components/Form"
+import Weather from "./components/Weather"
+
+const API_KEY = "45decf72ca92022978d10c03858bc9e0";
 
 class App extends React.Component {
+	getWeather = async () => {
+		const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
+		
+		const data = await api_call.json();
+	}
 	render(){
 		return(
       		<div>
 				<Titles />
 				<Form />
+				<Weather />
 			</div>
 		);
 	}
