2edf836e5b5458c7a2569009cf4d31b85ec08e9e
Karthik M S <ms18karthik@gmail.com>
2018-04-12 17:01:16 +0530

    Fetching data from the weather API on submit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import Weather from "./components/Weather"
 const API_KEY = "45decf72ca92022978d10c03858bc9e0";
 
 class App extends React.Component {
-	getWeather = async () => {
-		const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
-		
+	getWeather = async (e) => {
+		e.preventDefault();
+		const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Bengaluru,ind&appid=${API_KEY}&units=metric`);
+
 		const data = await api_call.json();
+
+		console.log(data);
 	}
 	render(){
 		return(
       		<div>
 				<Titles />
-				<Form />
+				<Form getWeather={this.getWeather}/>
 				<Weather />
 			</div>
 		);
@@ -23,3 +26,5 @@ class App extends React.Component {
 }
 
 export default App;
+
+[, 77.5913]
