359a4fde94f2fec52f306df250b5584a725e589a
Karthik M S <ms18karthik@gmail.com>
2018-06-08 12:49:31 +0530

    Writing media queries for smaller devices

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Weather = (props) => (
 	<div className="weather">
-		 {props.city && props.country && <p>Location: {props.city}, {props.country} <br/> <i className="fas fa-map-marker-alt"></i></p> }
-	     {props.temperature && <p>Temperature: {props.temperature} <br/><i className="fas fa-thermometer-half"></i></p>}
-	     {props.humidity && <p>Humidity: {props.humidity} <br/><i className="fas fa-tint"></i></p>}
-	     {props.description && <p>Description: {props.description} <br/> <i className="far fa-edit"></i></p>}
-	     {props.error && <p>{props.error} <br/> <i className="fas fa-exclamation-circle"></i></p>}
+		 {props.city && props.country && <p>Location: {props.city}, {props.country} <br/> <i className="fas fa-map-marker-alt fa-size"></i></p> }
+	     {props.temperature && <p>Temperature: {props.temperature} <br/><i className="fas fa-thermometer-half fa-size"></i></p>}
+	     {props.humidity && <p>Humidity: {props.humidity} <br/><i className="fas fa-tint fa-size"></i></p>}
+	     {props.description && <p>Description: {props.description} <br/> <i className="far fa-edit fa-size"></i></p>}
+	     {props.error && <p>{props.error} <br/> <i className="fas fa-exclamation-circle fa-size"></i></p>}
 	  </div>
 )
 
